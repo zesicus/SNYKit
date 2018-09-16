1dd8a2ecf25b2bdf9e233e57f10b5ba76699f714
Sunny <zesicus@163.com>
2018-09-16 17:00:19 +0800

    Add table view scroll to top or bottom..

---
+++ SNYKit/Classes/Extension/UITableView.swift
@@ -0,0 +1,26 @@
+//
+//  UITableView.swift
+//  Alamofire
+//
+//  Created by Sunny on 2018/9/16.
+//
+
+import UIKit
+
+extension UITableView {
+    
+    //滚动到顶部
+    public func scrollToTop(animated: Bool) {
+        self.setContentOffset(CGPoint(x: 0, y: 0), animated: animated)
+    }
+    
+    //滚动到底部
+    public func scrollToBot(animated: Bool) {
+        let s = self.numberOfSections
+        if (s < 1) {return}
+        let r = self.numberOfRows(inSection: s - 1)
+        if (r < 1) {return}
+        let ip = IndexPath(row: r - 1, section: s - 1)
+        self.scrollToRow(at: ip, at: .bottom, animated: animated)
+    }
+}
